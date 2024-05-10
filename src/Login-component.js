e6e6568e5b3a1717bdc6ed3cd32c71e9f958e385
D code <note37@naver.com>
2024-05-10 11:06:14 +0900

    로그인페이지 정렬

--- src/Login-component.js
+++ src/Login-component.js
@@ -1,13 +1,16 @@
+import { useState } from 'react';
 import './css/loginpage.css';
 
 const Loginpage = () => {
+  const [loginAccess, setLoginAccess] = useState("");
+  
   return (
     <div className="container">
       <div className="box">
         <p className="image-item"></p>
         <input type="email" placeholder="📧   Email" />
         <input type="password" placeholder="🔑   Password" />
-        <p>
+        <p className='loginSub'>
           <p><input type="checkbox"></input>Remeber ID</p>
           <a href="">For got Password?</a>
         </p>
